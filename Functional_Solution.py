25a333c13289495612063bdd380998ba269c3e43
Jack Jorgensen <jack.jorgensen@student.manchester.ac.uk>
2022-08-01 17:38:09 +0100

    Refactored names and time test

--- Functional_Solution.py
+++ Functional_Solution.py
@@ -1,18 +1,8 @@
 import pandas as pd
 import numpy as np
 # Mako 2022 Virtual Internship python project
-
-"""
-A function to print the results in the desired format
-# The input should be a dictionary, the key being the symbol and the value a dictionary
-with the min, max, average, and volume constituting the key value pairs
-    Parameters:
-        df (pandas.DataFrame): A data frame for a single given symbol
-
-    Returns:
-        None
-"""
 def print_output(symbol, max_price, min_price, average_price, total_volume):
+    """Prints formatted output of aggregates for given symbol."""
     print(f'Symbol: {symbol}, Max Price: {max_price:4}, Min Price: {min_price:4}, '\
             f'Average Price: {average_price:7.2f}, Total Volume: {total_volume:5}')
 
@@ -20,7 +10,8 @@ def calc_aggregates(df):
     return df["Price"].max(), df["Price"].min(), df["Price"].mean(), df["Volume"].sum()
 
 
-def main(input_file):
+def main():
+    input_file = 'https://storage.googleapis.com/temp-bucket-5862/input_data.csv'
     input_headers = ["Timestamp", "Symbol", "Volume", "Price"]
     #load into a data frame
     trades_df = pd.read_csv(input_file, names=input_headers)
@@ -33,4 +24,4 @@ def main(input_file):
 if __name__ == "__main__":
     file2 = "./input.csv"
     file = 'https://storage.googleapis.com/temp-bucket-5862/input_data.csv'
-    main(file)
+    main()
--- time_test.py
+++ time_test.py
@@ -1,9 +1,9 @@
 from tracemalloc import start
-import Skeleton
-import main
+import OOP_Solution
+import Functional_Solution
 import time
 
-def time_it(function):
+def time_it(functions):
     times = []
     for x in range(100):
         start_time = time.time_ns()
@@ -12,9 +12,8 @@ def time_it(function):
         total = end_time - start_time
         times.append(total/1000000000)
 
-    print(f"Function: {str(function)} Min: {min(times)}, Max: {max(times)}, Average: {sum(times)/len(times):.2f},'\
-        ' Total: {sum(times)}")
+    print(f'Function: {function.__name__} Min: {min(times)}, Max: {max(times)}, Average: {sum(times)/len(times):.2f},'\
+        f' Total: {sum(times)}')
 
 if __name__ == '__main__':
-    time_it(Skeleton.main)
-    time_it(main.main)
+    time_it([OOP_Solution.main, Functional_Solution.main])
